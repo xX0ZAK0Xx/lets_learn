b1867a8af3c4bffe43928325706b27dfd0d1ac56
zayed-m360 <zayedoyshik.m360@gmail.com>
2025-05-10 16:37:51 +0600

    refactor: remove phone number authentication logic from AuthBloc and related states

--- lib/blocs/bloc/auth_event.dart
+++ lib/blocs/bloc/auth_event.dart
@@ -2,16 +2,4 @@ part of 'auth_bloc.dart';
 
 sealed class AuthEvent {}
 
-class PhoneNumberAuthEvent extends AuthEvent {
-  final String phoneNumber;
-
-  PhoneNumberAuthEvent({required this.phoneNumber});
-}
-
-class VerifyOTPEvent extends AuthEvent {
-  final String otp, verificationId;
-
-  VerifyOTPEvent({required this.otp, required this.verificationId});
-}
-
 class GoogleAuthEvent extends AuthEvent{}
--- lib/blocs/bloc/auth_state.dart
+++ lib/blocs/bloc/auth_state.dart
@@ -4,36 +4,10 @@ sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
 
-//? Phone Number Auth States
-final class PhoneNumberAuthLoadingState extends AuthState {}
-
-final class PhoneNumberAuthSuccessState extends AuthState {
-  final String verificationId, phonNumber;
-
-  PhoneNumberAuthSuccessState({required this.verificationId, required this.phonNumber});
-}
-
-final class PhoneNumberAuthFailedState extends AuthState {
-  final String errorMessage;
-
-  PhoneNumberAuthFailedState({required this.errorMessage});
-}
-
-// Verify OTP State
-final class VerifyOtpLoadingState extends AuthState {}
-
-final class VerifyOtpSuccessState extends AuthState {}
-
-final class VerifyOtpFailedState extends AuthState {
-  final String errorMessage;
-
-  VerifyOtpFailedState({required this.errorMessage});
-}
-
 // Google Auth States
 final class GoogleAuthLoadingState extends AuthState {}
 
-class GoogleAuthSuccessState extends AuthState {
+final class GoogleAuthSuccessState extends AuthState {
   final User user;
 
   GoogleAuthSuccessState({required this.user});
